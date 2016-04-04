d6f1bdbaf4bbd3bf0d187d94a067d798e4bf7eb8
darespaco <darespaco@MacBook-Pro-de-Diego.local>
2016-04-04 13:24:53 +0200

    fixed config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'parlaproject');
+define('DB_NAME', 'u998692852_parla');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'u998692852_parla');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'cientoporuno_');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.hostinger.es');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
